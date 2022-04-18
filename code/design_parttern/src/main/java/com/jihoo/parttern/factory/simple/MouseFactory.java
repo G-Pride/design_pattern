c0f05abfa73723d43002debf7b83236b1c2286c3
guozhihao <g13692021053>
2022-04-18 14:10:02 +0800

    简单工厂

--- code/design_parttern/src/main/java/com/jihoo/parttern/factory/simple/MouseFactory.java
+++ code/design_parttern/src/main/java/com/jihoo/parttern/factory/simple/MouseFactory.java
@@ -8,6 +8,13 @@ import com.jihoo.parttern.factory.service.impl.LenovoMouse;
 /**
  * @Author：guozhihao
  * @Date：2022/4/18 1:20 下午
+ *
+ * 简单工厂模式
+ * 优点：可以对创建对象进行加工，对客户端隐藏细节。
+ * 缺点：1、新增、删除子类均会违反【开闭原则】；
+ *      2、创建对象过多造成代码臃肿。
+ * 使用场景：1、创建对象的场景较少；
+ *         2、客户端不关心对象的创建过程。
  */
 public class MouseFactory {
 
